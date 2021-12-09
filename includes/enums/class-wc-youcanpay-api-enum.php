151a02612c21da88919ebea50b3febc63379c868
Mohamed Oussama EL ABBASSI <med.oussama@nextmedia.ma>
2021-12-09 12:16:48 +0100

    DOT-4682: Need to remove domain test and dev mode from API

--- includes/enums/class-wc-youcanpay-api-enum.php
+++ includes/enums/class-wc-youcanpay-api-enum.php
@@ -1,37 +1,32 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
-class WC_YouCanPay_Api_Enum {
+class WC_YouCanPay_Api_Enum
+{
 
-	private static $base_url_test = 'https://pay.testyoucan.shop';
-	private static $base_url_production = 'https://pay.youcan.shop';
+    /** @var string */
+    private static $base_url = 'https://pay.youcan.shop';
 
-	private static $javascript_path = '/js/ycpay.js';
+    /** @var string */
+    private static $javascript_path = '/js/ycpay.js';
 
-	/**
-	 * @return string
-	 */
-	public static function get_base_url(): string {
-		if (WC_YOUCAN_PAY_MODE_DEV !== '1') {
-			return self::$base_url_production;
-		}
+    /**
+     * @return string
+     */
+    public static function get_base_url(): string
+    {
+        return self::$base_url;
+    }
 
-		return self::$base_url_test;
-	}
-
-	/**
-	 * @return string
-	 */
-	public static function get_javascript_url(): string {
-		if (WC_YOUCAN_PAY_MODE_DEV !== '1') {
-			return self::$base_url_production . self::$javascript_path;
-		}
-
-		return self::$base_url_test . self::$javascript_path;
-	}
-
+    /**
+     * @return string
+     */
+    public static function get_javascript_url(): string
+    {
+        return self::$base_url . self::$javascript_path;
+    }
 
 }
