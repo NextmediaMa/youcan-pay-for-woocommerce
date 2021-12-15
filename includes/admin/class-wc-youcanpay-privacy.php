0e2c86c9f2022a149fa64a13d7429798e403e951
Mohamed Oussama EL ABBASSI <med.oussama@nextmedia.ma>
2021-12-15 15:00:17 +0100

    DOT-4686: Managing WooCommerce YouCan Pay Privacy

--- includes/admin/class-wc-youcanpay-privacy.php
+++ includes/admin/class-wc-youcanpay-privacy.php
@@ -11,65 +11,15 @@ class WC_YouCanPay_Privacy extends WC_Abstract_Privacy {
 	public function __construct() {
 		parent::__construct( __( 'YouCan Pay', 'youcan-pay' ) );
 
-		$this->add_exporter(
-			'youcan-pay-order-data',
+		$this->add_exporter( 'youcan-pay-order-data',
 			__( 'YouCan Pay Order Data', 'youcan-pay' ),
 			[ $this, 'order_data_exporter' ]
-		);
-		$this->add_exporter(
-			'youcan-pay-customer-data',
-			__( 'YouCan Pay Customer Data', 'youcan-pay' ),
-			[ $this, 'customer_data_exporter' ]
-		);
-		$this->add_eraser(
-			'youcan-pay-customer-data',
-			__( 'YouCan Pay Customer Data', 'youcan-pay' ),
-			[ $this, 'customer_data_eraser' ]
 		);
 		$this->add_eraser(
 			'youcan-pay-order-data',
 			__( 'YouCan Pay Data', 'youcan-pay' ),
 			[ $this, 'order_data_eraser' ]
 		);
-
-		add_filter( 'woocommerce_get_settings_account', [ $this, 'account_settings' ] );
-	}
-
-	/**
-	 * Add retention settings to account tab.
-	 *
-	 * @param array $settings
-	 *
-	 * @return array $settings Updated
-	 */
-	public function account_settings( $settings ) {
-		$insert_setting = [
-			[
-				'title'       => __( 'Retain YouCan Pay Data', 'youcan-pay' ),
-				'desc_tip'    => __( 'Retains any YouCan Pay data such as YouCan Pay customer ID, source ID.',
-					'youcan-pay' ),
-				'id'          => 'woocommerce_gateway_youcanpay_retention',
-				'type'        => 'relative_date_selector',
-				'placeholder' => __( 'N/A', 'youcan-pay' ),
-				'default'     => '',
-				'autoload'    => false,
-			],
-		];
-
-		$index = null;
-
-		foreach ( $settings as $key => $value ) {
-			if ( 'sectionend' === $value['type'] && 'personal_data_retention' === $value['id'] ) {
-				$index = $key;
-				break;
-			}
-		}
-
-		if ( ! is_null( $index ) ) {
-			array_splice( $settings, $index, 0, $insert_setting );
-		}
-
-		return $settings;
 	}
 
 	/**
@@ -132,12 +82,8 @@ class WC_YouCanPay_Privacy extends WC_Abstract_Privacy {
 					'item_id'     => 'order-' . $order->get_id(),
 					'data'        => [
 						[
-							'name'  => __( 'YouCan Pay payment id', 'youcan-pay' ),
+							'name'  => __( 'YouCan Pay Transaction ID', 'youcan-pay' ),
 							'value' => get_post_meta( $order->get_id(), '_youcanpay_source_id', true ),
-						],
-						[
-							'name'  => __( 'YouCan Pay customer id', 'youcan-pay' ),
-							'value' => get_post_meta( $order->get_id(), '_youcanpay_customer_id', true ),
 						],
 					],
 				];
@@ -149,76 +95,6 @@ class WC_YouCanPay_Privacy extends WC_Abstract_Privacy {
 		return [
 			'data' => $data_to_export,
 			'done' => $done,
-		];
-	}
-
-	/**
-	 * Finds and exports customer data by email address.
-	 *
-	 * @param string $email_address The user email address.
-	 * @param int $page Page.
-	 *
-	 * @return array An array of personal data in name value pairs
-	 */
-	public function customer_data_exporter( $email_address, $page ) {
-		$user           = get_user_by( 'email',
-			$email_address ); // Check if user has an ID in the DB to load stored personal data.
-		$data_to_export = [];
-
-		if ( $user instanceof WP_User ) {
-			$data_to_export[] = [
-				'group_id'    => 'woocommerce_customer',
-				'group_label' => __( 'Customer Data', 'youcan-pay' ),
-				'item_id'     => 'user',
-				'data'        => [
-					[
-						'name'  => __( 'YouCan Pay payment id', 'youcan-pay' ),
-						'value' => get_user_option( '_youcanpay_source_id', $user->ID ),
-					]
-				],
-			];
-		}
-
-		return [
-			'data' => $data_to_export,
-			'done' => true,
-		];
-	}
-
-	/**
-	 * Finds and erases customer data by email address.
-	 *
-	 * @param string $email_address The user email address.
-	 * @param int $page Page.
-	 *
-	 * @return array An array of personal data in name value pairs
-	 */
-	public function customer_data_eraser( $email_address, $page ) {
-		// Check if user has an ID in the DB to load stored personal data.
-		$user                  = get_user_by( 'email', $email_address );
-		$youcanpay_customer_id = '';
-		$youcanpay_source_id   = '';
-
-		if ( $user instanceof WP_User ) {
-			$youcanpay_customer_id = get_user_option( '_youcanpay_customer_id', $user->ID );
-			$youcanpay_source_id   = get_user_option( '_youcanpay_source_id', $user->ID );
-		}
-
-		$items_removed = false;
-		$messages      = [];
-
-		if ( ! empty( $youcanpay_customer_id ) || ! empty( $youcanpay_source_id ) ) {
-			$items_removed = true;
-			delete_user_option( $user->ID, '_youcanpay_customer_id' );
-			delete_user_option( $user->ID, '_youcanpay_source_id' );
-			$messages[] = __( 'YouCan Pay User Data Erased.', 'youcan-pay' );
-		}
-
-		return [
-			'items_removed'  => $items_removed,
-			'items_retained' => false,
-			'messages'       => $messages,
-			'done'           => true,
 		];
 	}
 
@@ -265,10 +141,8 @@ class WC_YouCanPay_Privacy extends WC_Abstract_Privacy {
 	 * @return array
 	 */
 	protected function maybe_handle_order( $order ) {
-		$order_id              = $order->get_id();
-		$youcanpay_source_id   = get_post_meta( $order_id, '_youcanpay_source_id', true );
-		$youcanpay_refund_id   = get_post_meta( $order_id, '_youcanpay_refund_id', true );
-		$youcanpay_customer_id = get_post_meta( $order_id, '_youcanpay_customer_id', true );
+		$order_id            = $order->get_id();
+		$youcanpay_source_id = get_post_meta( $order_id, '_youcanpay_source_id', true );
 
 		if ( ! $this->is_retention_expired( $order->get_date_created()->getTimestamp() ) ) {
 			/* translators: %d Order ID */
@@ -276,20 +150,20 @@ class WC_YouCanPay_Privacy extends WC_Abstract_Privacy {
 				false,
 				true,
 				[
-					sprintf( __( 'Order ID %d is less than set retention days. Personal data retained. (YouCan Pay)',
-						'youcan-pay' ),
-						$order->get_id() )
+					sprintf(
+						__( 'Order ID %d is less than set retention days. Personal data retained. (YouCan Pay)',
+							'youcan-pay' ),
+						$order->get_id()
+					)
 				]
 			];
 		}
 
-		if ( empty( $youcanpay_source_id ) && empty( $youcanpay_refund_id ) && empty( $youcanpay_customer_id ) ) {
+		if ( empty( $youcanpay_source_id ) ) {
 			return [ false, false, [] ];
 		}
 
 		delete_post_meta( $order_id, '_youcanpay_source_id' );
-		delete_post_meta( $order_id, '_youcanpay_refund_id' );
-		delete_post_meta( $order_id, '_youcanpay_customer_id' );
 
 		return [ true, false, [ __( 'YouCan Pay personal data erased.', 'youcan-pay' ) ] ];
 	}
