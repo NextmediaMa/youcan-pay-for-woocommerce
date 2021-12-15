bed2f80778af4957fea82ef435ba424548b56d3a
Mohamed Oussama EL ABBASSI <med.oussama@nextmedia.ma>
2021-12-15 16:41:34 +0100

    DOT-4690: code review 1

--- includes/enums/class-wc-youcanpay-order-action-action-enum.php
+++ includes/enums/class-wc-youcanpay-order-action-action-enum.php
@@ -1,41 +1,44 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
 /**
  * Order_Action (Order Page): This is the page from which the user clicked on the Pay action
  * We are free to modify these values
  */
-class WC_YouCanPay_Order_Action_Enum {
+class WC_YouCanPay_Order_Action_Enum
+{
 
-	private static $incomplete = 1;
-	private static $pre_order = 2;
+    private static $incomplete = 1;
+    private static $pre_order = 2;
 
-	/**
-	 * @return string
-	 */
-	public static function get_incomplete(): string {
-		return self::$incomplete;
-	}
+    /**
+     * @return string
+     */
+    public static function get_incomplete(): string
+    {
+        return self::$incomplete;
+    }
 
-	/**
-	 * @return string
-	 */
-	public static function get_pre_order(): string {
-		return self::$pre_order;
-	}
+    /**
+     * @return string
+     */
+    public static function get_pre_order(): string
+    {
+        return self::$pre_order;
+    }
 
     /**
      * @return array
      */
-	public static function get_all(): array
+    public static function get_all(): array
     {
-		return array(
-			self::$incomplete,
-			self::$pre_order,
-		);
-	}
+        return [
+            self::$incomplete,
+            self::$pre_order,
+        ];
+    }
 
 }
