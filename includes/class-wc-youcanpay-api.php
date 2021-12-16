b26e5a55adbfa83e9182d0144de2bff6073176c9
Mohamed Oussama EL ABBASSI <med.oussama@nextmedia.ma>
2021-12-16 10:26:30 +0100

    DOT-4767: When using Standalone mode, the user should be directed to the payment page with the language of the store (#12)
    * DOT-4767: When using Standalone mode, the user should be directed to the payment page with the language of the store
    
    * DOT-4767: code review 1
    
    * DOT-4767: code review 2
    
    * DOT-4767: code review 3

--- includes/class-wc-youcanpay-api.php
+++ includes/class-wc-youcanpay-api.php
@@ -4,8 +4,8 @@ use YouCan\Pay\Models\Token;
 use YouCan\Pay\Models\Transaction;
 use YouCan\Pay\YouCanPay;
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
 /**
@@ -13,238 +13,261 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * Communicates with YouCan Pay API.
  */
-class WC_YouCanPay_API {
+class WC_YouCanPay_API
+{
+    /**
+     * Secret API Key.
+     *
+     * @var string
+     */
+    private static $private_key = '';
 
-	/**
-	 * Secret API Key.
-	 *
-	 * @var string
-	 */
-	private static $private_key = '';
+    /**
+     * Secret API Key.
+     *
+     * @var string
+     */
+    private static $public_key = '';
 
-	/**
-	 * Secret API Key.
-	 *
-	 * @var string
-	 */
-	private static $public_key = '';
+    /**
+     * Sandbox Mode is enabled.
+     *
+     * @var string
+     */
+    private static $is_test_mode = '';
 
-	/**
-	 * Sandbox Mode is enabled.
-	 *
-	 * @var string
-	 */
-	private static $is_test_mode = '';
+    /**
+     * Set secret API Key.
+     *
+     * @param string $private_key
+     */
+    public static function set_private_key($private_key)
+    {
+        self::$private_key = $private_key;
+    }
 
-	/**
-	 * Set secret API Key.
-	 *
-	 * @param string $private_key
-	 */
-	public static function set_private_key( $private_key ) {
-		self::$private_key = $private_key;
-	}
+    /**
+     * Set secret API Key.
+     *
+     * @param string $public_key
+     */
+    public static function set_public_key($public_key)
+    {
+        self::$public_key = $public_key;
+    }
 
-	/**
-	 * Set secret API Key.
-	 *
-	 * @param string $public_key
-	 */
-	public static function set_public_key( $public_key ) {
-		self::$public_key = $public_key;
-	}
+    /**
+     * Set secret API Key.
+     *
+     * @param string $test_mode
+     */
+    public static function set_test_mode($test_mode)
+    {
+        self::$is_test_mode = ('yes' === $test_mode);
+    }
 
-	/**
-	 * Set secret API Key.
-	 *
-	 * @param string $test_mode
-	 */
-	public static function set_test_mode( $test_mode ) {
-		self::$is_test_mode = ( 'yes' === $test_mode );
-	}
+    /**
+     * Get secret key.
+     *
+     * @return string
+     */
+    public static function get_private_key()
+    {
+        if (!self::$private_key) {
+            $options = get_option('woocommerce_youcanpay_settings');
 
-	/**
-	 * Get secret key.
-	 *
-	 * @return string
-	 */
-	public static function get_private_key() {
-		if ( ! self::$private_key ) {
-			$options = get_option( 'woocommerce_youcanpay_settings' );
+            if (isset($options['sandbox_mode'], $options['private_key'], $options['sandbox_private_key'])) {
+                self::set_private_key(
+                    'yes' === $options['sandbox_mode'] ? $options['sandbox_private_key'] : $options['private_key']
+                );
+            }
+        }
 
-			if ( isset( $options['sandbox_mode'], $options['private_key'], $options['sandbox_private_key'] ) ) {
-				self::set_private_key( 'yes' === $options['sandbox_mode'] ? $options['sandbox_private_key'] : $options['private_key'] );
-			}
-		}
+        return self::$private_key;
+    }
 
-		return self::$private_key;
-	}
+    /**
+     * Get public key.
+     *
+     * @return string
+     */
+    public static function get_public_key()
+    {
+        if (!self::$public_key) {
+            $options = get_option('woocommerce_youcanpay_settings');
 
-	/**
-	 * Get public key.
-	 *
-	 * @return string
-	 */
-	public static function get_public_key() {
-		if ( ! self::$public_key ) {
-			$options = get_option( 'woocommerce_youcanpay_settings' );
+            if (isset($options['sandbox_mode'], $options['public_key'], $options['sandbox_public_key'])) {
+                self::set_public_key(
+                    'yes' === $options['sandbox_mode'] ? $options['sandbox_public_key'] : $options['public_key']
+                );
+            }
+        }
 
-			if ( isset( $options['sandbox_mode'], $options['public_key'], $options['sandbox_public_key'] ) ) {
-				self::set_public_key( 'yes' === $options['sandbox_mode'] ? $options['sandbox_public_key'] : $options['public_key'] );
-			}
-		}
+        return self::$public_key;
+    }
 
-		return self::$public_key;
-	}
+    /**
+     * Get public key.
+     *
+     * @return string
+     */
+    public static function is_test_mode()
+    {
+        if ('' === self::$is_test_mode) {
+            $options = get_option('woocommerce_youcanpay_settings');
 
-	/**
-	 * Get public key.
-	 *
-	 * @return string
-	 */
-	public static function is_test_mode() {
-		if ( '' === self::$is_test_mode ) {
-			$options = get_option( 'woocommerce_youcanpay_settings' );
+            if (isset($options['sandbox_mode'])) {
+                self::set_test_mode($options['sandbox_mode']);
+            }
+        }
 
-			if ( isset( $options['sandbox_mode'] ) ) {
-				self::set_test_mode( $options['sandbox_mode'] );
-			}
-		}
+        return self::$is_test_mode;
+    }
 
-		return self::$is_test_mode;
-	}
+    /**
+     * Send the request to YouCan Pay's API
+     *
+     * @param WC_Order|WC_Order_Refund $order
+     * @param array $post_data
+     *
+     * @return stdClass|array
+     * @throws WC_YouCanPay_Exception
+     */
+    public static function request($order, $post_data)
+    {
+        $response = [
+            'id'       => 0,
+            'redirect' => [
+                'url' => get_home_url(),
+            ],
+        ];
 
-	/**
-	 * Send the request to YouCan Pay's API
-	 *
-	 * @param WC_Order|WC_Order_Refund $order
-	 * @param array $post_data
-	 *
-	 * @return stdClass|array
-	 */
-	public static function request( $order, $post_data ) {
-		$response = [
-			'id'       => 0,
-			'redirect' => [
-				'url' => get_home_url()
-			]
-		];
+        try {
+            if (self::is_test_mode()) {
+                YouCanPay::setIsSandboxMode(true);
+            }
+            $py = YouCanPay::instance()->useKeys(
+                self::get_private_key(),
+                self::get_public_key()
+            );
 
-		try {
-			if ( self::is_test_mode() ) {
-				YouCanPay::setIsSandboxMode( true );
-			}
-			$py = YouCanPay::instance()->useKeys(
-				self::get_private_key(),
-				self::get_public_key()
-			);
+            $token = $py->token->create(
+                $order->get_id(),
+                (int)$post_data['amount'],
+                $post_data['currency'],
+                self::get_the_user_ip(),
+                $post_data['redirect']['return_url'],
+                $post_data['redirect']['return_url']
+            );
 
-			$token = $py->token->create(
-				$order->get_id(),
-				(int) $post_data['amount'],
-				$post_data['currency'],
-				self::get_the_user_ip(),
-				$post_data['redirect']['return_url'],
-				$post_data['redirect']['return_url']
-			);
+            if (is_wp_error($token) || empty($token)) {
+                throw new WC_YouCanPay_Exception(
+                    print_r($token, true),
+                    __('There was a problem connecting to the YouCan Pay API endpoint.', 'youcan-pay')
+                );
+            }
 
-			if ( is_wp_error( $token ) || empty( $token ) ) {
-				WC_YouCanPay_Logger::info( 'there was a problem connecting to the YouCan Pay API endpoint', array(
-					'order_id' => $order->get_id(),
-				) );
+            $response = [
+                'id'       => $token->getId(),
+                'redirect' => [
+                    'url' => $token->getPaymentURL($post_data['locale']),
+                ],
+            ];
+        } catch (WC_YouCanPay_Exception $e) {
+            WC_YouCanPay_Logger::alert('there was a problem connecting to the YouCan Pay API endpoint', [
+                'order_id' => $order->get_id(),
+            ]);
+            throw new WC_YouCanPay_Exception($e->getMessage(), $e->getLocalizedMessage());
+        } catch (Throwable $e) {
+            WC_YouCanPay_Logger::alert('throwable at request exists into wc youcan pay api', [
+                'exception.message' => $e->getMessage(),
+            ]);
+        }
 
-				throw new WC_YouCanPay_Exception( print_r( $token, true ),
-					__( 'There was a problem connecting to the YouCan Pay API endpoint.', 'youcan-pay' ) );
-			}
+        return json_decode(json_encode($response));
+    }
 
-			$response = [
-				'id'       => $token->getId(),
-				'redirect' => [
-					'url' => $token->getPaymentURL()
-				]
-			];
-		} catch (Throwable $e) {
-			WC_YouCanPay_Logger::alert( 'throwable at request exists into wc youcan pay api', array(
-				'exception.message' => $e->getMessage()
-			) );
-		}
+    /**
+     * @param $transaction_id
+     *
+     * @return Transaction|null
+     */
+    public static function get_transaction($transaction_id)
+    {
+        try {
+            if (self::is_test_mode()) {
+                YouCanPay::setIsSandboxMode(true);
+            }
+            $py = YouCanPay::instance()->useKeys(
+                self::get_private_key(),
+                self::get_public_key()
+            );
 
-		return json_decode( json_encode( $response ) );
-	}
+            return $py->transaction->get($transaction_id);
+        } catch (Throwable $e) {
+            WC_YouCanPay_Logger::alert('throwable at get transaction exists into wc youcan pay api', [
+                'exception.message' => $e->getMessage(),
+            ]);
+        }
 
-	/**
-	 * @return Transaction|null
-	 */
-	public static function get_transaction( $transaction_id ) {
-		try {
-			if ( self::is_test_mode() ) {
-				YouCanPay::setIsSandboxMode( true );
-			}
-			$py = YouCanPay::instance()->useKeys(
-				self::get_private_key(),
-				self::get_public_key()
-			);
+        return null;
+    }
 
-			return $py->transaction->get( $transaction_id );
-		} catch (Throwable $e) {
-			WC_YouCanPay_Logger::alert( 'throwable at get transaction exists into wc youcan pay api', array(
-				'exception.message' => $e->getMessage()
-			) );
-		}
+    /**
+     * @param $order WC_Order
+     * @param $total
+     * @param $currency
+     * @param $return_url
+     *
+     * @return Token|null
+     */
+    public static function create_token($order, $total, $currency, $return_url)
+    {
+        try {
+            $amount = WC_YouCanPay_Helper::get_youcanpay_amount($total, $currency);
 
-		return null;
-	}
+            if (self::is_test_mode()) {
+                YouCanPay::setIsSandboxMode(true);
+            }
 
-	/**
-	 * @param $order WC_Order
-	 * @param $total
-	 * @param $currency
-	 * @param $return_url
-	 *
-	 * @return Token|null
-	 */
-	public static function create_token( $order, $total, $currency, $return_url ) {
-		try {
-			$amount = WC_YouCanPay_Helper::get_youcanpay_amount( $total, $currency );
+            $py = YouCanPay::instance()->useKeys(
+                self::get_private_key(),
+                self::get_public_key()
+            );
 
-			if ( self::is_test_mode() ) {
-				YouCanPay::setIsSandboxMode( true );
-			}
+            return $py->token->create(
+                $order->get_id(),
+                $amount,
+                strtoupper($currency),
+                self::get_the_user_ip(),
+                $return_url,
+                $return_url
+            );
+        } catch (Throwable $e) {
+            WC_YouCanPay_Logger::alert('throwable at create token exists into wc youcan pay api', [
+                'exception.message' => $e->getMessage(),
+            ]);
+        }
 
-			$py = YouCanPay::instance()->useKeys(
-				self::get_private_key(),
-				self::get_public_key()
-			);
+        return null;
+    }
 
-			return $py->token->create(
-				$order->get_id(),
-				$amount,
-				strtoupper( $currency ),
-				self::get_the_user_ip(),
-				$return_url,
-				$return_url
-			);
-		} catch ( Throwable $e ) {
-			WC_YouCanPay_Logger::alert( 'throwable at create token exists into wc youcan pay api', array(
-				'exception.message' => $e->getMessage()
-			) );
-		}
+    /**
+     * @return string
+     */
+    public static function get_the_user_ip()
+    {
+        //check ip from share internet
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            return $_SERVER['HTTP_CLIENT_IP'];
+        }
 
-		return null;
-	}
+        //to check ip is pass from proxy
+        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
 
-	public static function get_the_user_ip() {
-		if ( ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
-			//check ip from share internet
-			$ip = $_SERVER['HTTP_CLIENT_IP'];
-		} elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
-			//to check ip is pass from proxy
-			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-		} else {
-			$ip = $_SERVER['REMOTE_ADDR'];
-		}
-
-		return $ip;
-	}
+        return $_SERVER['REMOTE_ADDR'];
+    }
 
 }
--- includes/payment-methods/class-wc-gateway-youcanpay-standalone.php
+++ includes/payment-methods/class-wc-gateway-youcanpay-standalone.php
@@ -1,7 +1,7 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
 /**
@@ -9,230 +9,225 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @extends WC_Gateway_YouCanPay
  */
-class WC_Gateway_YouCanPay_Standalone extends WC_YouCanPay_Payment_Gateway {
+class WC_Gateway_YouCanPay_Standalone extends WC_YouCanPay_Payment_Gateway
+{
 
-	const ID = 'youcanpay_standalone';
+    const ID = 'youcanpay_standalone';
 
-	/**
-	 * Notices (array)
-	 *
-	 * @var array
-	 */
-	public $notices = [];
+    /**
+     * Notices (array)
+     *
+     * @var array
+     */
+    public $notices = [];
 
-	/**
-	 * Is sandbox mode active?
-	 *
-	 * @var bool
-	 */
-	public $sandbox_mode;
+    /**
+     * Is sandbox mode active?
+     *
+     * @var bool
+     */
+    public $sandbox_mode;
 
-	/**
-	 * API access secret key
-	 *
-	 * @var string
-	 */
-	public $private_key;
+    /**
+     * API access secret key
+     *
+     * @var string
+     */
+    public $private_key;
 
-	/**
-	 * Api access publishable key
-	 *
-	 * @var string
-	 */
-	public $public_key;
+    /**
+     * Api access publishable key
+     *
+     * @var string
+     */
+    public $public_key;
 
-	/**
-	 * Constructor
-	 */
-	public function __construct() {
-		$this->id           = self::ID;
-		$this->method_title = __( 'YouCan Pay Standalone', 'youcan-pay' );
-		/* translators: link */
-		$this->method_description = sprintf(
-			__( 'All other general YouCan Pay settings can be adjusted <a href="%s">here</a>.', 'youcan-pay' ),
-			admin_url( 'admin.php?page=wc-settings&tab=checkout&section=youcanpay' )
-		);
-		$this->supports           = [
-			'products',
-			'refunds',
-		];
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->id = self::ID;
+        $this->method_title = __('YouCan Pay Standalone', 'youcan-pay');
+        /* translators: link */
+        $this->method_description = sprintf(
+            __('All other general YouCan Pay settings can be adjusted <a href="%s">here</a>.', 'youcan-pay'),
+            admin_url('admin.php?page=wc-settings&tab=checkout&section=youcanpay')
+        );
+        $this->supports = [
+            'products',
+            'refunds',
+        ];
 
-		// Load the form fields.
-		$this->init_form_fields();
+        // Load the form fields.
+        $this->init_form_fields();
 
-		// Load the settings.
-		$this->init_settings();
+        // Load the settings.
+        $this->init_settings();
 
-		$main_settings      = get_option( 'woocommerce_youcanpay_settings' );
-		$this->title        = $this->get_option( 'title' );
-		$this->description  = $this->get_option( 'description' );
-		$this->enabled      = $this->get_option( 'enabled' );
-		$this->sandbox_mode = ! empty( $main_settings['sandbox_mode'] ) && 'yes' === $main_settings['sandbox_mode'];
-		$this->public_key   = ! empty( $main_settings['public_key'] ) ? $main_settings['public_key'] : '';
-		$this->private_key  = ! empty( $main_settings['private_key'] ) ? $main_settings['private_key'] : '';
+        $main_settings = get_option('woocommerce_youcanpay_settings');
+        $this->title = $this->get_option('title');
+        $this->description = $this->get_option('description');
+        $this->enabled = $this->get_option('enabled');
+        $this->sandbox_mode = !empty($main_settings['sandbox_mode']) && 'yes' === $main_settings['sandbox_mode'];
+        $this->public_key = !empty($main_settings['public_key']) ? $main_settings['public_key'] : '';
+        $this->private_key = !empty($main_settings['private_key']) ? $main_settings['private_key'] : '';
 
-		if ( $this->sandbox_mode ) {
-			$this->public_key  = ! empty( $main_settings['sandbox_public_key'] ) ? $main_settings['sandbox_public_key'] : '';
-			$this->private_key = ! empty( $main_settings['sandbox_private_key'] ) ? $main_settings['sandbox_private_key'] : '';
-		}
+        if ($this->sandbox_mode) {
+            $this->public_key = !empty($main_settings['sandbox_public_key']) ? $main_settings['sandbox_public_key'] : '';
+            $this->private_key = !empty($main_settings['sandbox_private_key']) ? $main_settings['sandbox_private_key'] : '';
+        }
 
-		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, [ $this, 'process_admin_options' ] );
-		add_action( 'wp_enqueue_scripts', [ $this, 'payment_scripts' ] );
-	}
+        add_action('woocommerce_update_options_payment_gateways_' . $this->id, [$this, 'process_admin_options']);
+        add_action('wp_enqueue_scripts', [$this, 'payment_scripts']);
+    }
 
-	/**
-	 * Checks to see if all criteria is met before showing payment method.
-	 *
-	 * @return bool
-	 */
-	public function is_available() {
-        if (! in_array(get_woocommerce_currency(), $this->get_supported_currency())) {
+    /**
+     * Checks to see if all criteria is met before showing payment method.
+     *
+     * @return bool
+     */
+    public function is_available()
+    {
+        if (!in_array(get_woocommerce_currency(), $this->get_supported_currency())) {
             return false;
         }
 
-		return parent::is_available();
-	}
+        return parent::is_available();
+    }
 
-	/**
-	 * Get_icon function.
-	 *
-	 * @return string
-	 */
-	public function get_icon() {
-		$icons = $this->payment_icons();
+    /**
+     * Get_icon function.
+     *
+     * @return string
+     */
+    public function get_icon()
+    {
+        $icons = $this->payment_icons();
 
-		$icons_str = '';
-		$icons_str .= $icons['standalone'] ?? '';
+        $icons_str = '';
+        $icons_str .= $icons['standalone'] ?? '';
 
-		return apply_filters( 'woocommerce_gateway_icon', $icons_str, $this->id );
-	}
+        return apply_filters('woocommerce_gateway_icon', $icons_str, $this->id);
+    }
 
-	/**
-	 * Payment_scripts function.
-	 */
-	public function payment_scripts() {
-		if ( ! WC_YouCanPay_Helper::has_cart_or_checkout_on_current_page()
-		     && ! array_key_exists( 'pay_for_order', $_GET )
-		     && ! is_add_payment_method_page()
-		) {
-			return;
-		}
+    /**
+     * Payment_scripts function.
+     */
+    public function payment_scripts()
+    {
+        if (!WC_YouCanPay_Helper::has_cart_or_checkout_on_current_page()
+            && !array_key_exists('pay_for_order', $_GET)
+            && !is_add_payment_method_page()
+        ) {
+            return;
+        }
 
-		wp_enqueue_style( 'youcanpay_styles' );
-		wp_enqueue_script( 'woocommerce_youcanpay' );
-	}
+        wp_enqueue_style('youcanpay_styles');
+        wp_enqueue_script('woocommerce_youcanpay');
+    }
 
-	/**
-	 * Initialize Gateway Settings Form Fields.
-	 */
-	public function init_form_fields() {
-		$this->form_fields = require WC_YOUCAN_PAY_PLUGIN_PATH . '/includes/admin/youcanpay-standalone-settings.php';
-	}
+    /**
+     * Initialize Gateway Settings Form Fields.
+     */
+    public function init_form_fields()
+    {
+        $this->form_fields = require WC_YOUCAN_PAY_PLUGIN_PATH . '/includes/admin/youcanpay-standalone-settings.php';
+    }
 
-	/**
-	 * Payment form on checkout page
-	 */
-	public function payment_fields() {
-		$description = $this->get_description();
-        $description = ! empty( $description ) ? $description : '';
+    /**
+     * Payment form on checkout page
+     */
+    public function payment_fields()
+    {
+        $description = $this->get_description();
+        $description = !empty($description) ? $description : '';
 
         ob_start();
 
         echo '<div id="youcanpay-payment-data-standalone">';
 
-        $description = trim( $description );
-        echo wpautop( wp_kses_post( $description ) );
+        $description = trim($description);
+        echo wpautop(wp_kses_post($description));
 
-		echo '</div>';
+        echo '</div>';
 
         ob_end_flush();
-	}
+    }
 
-	/**
-	 * Creates the source for charge.
-	 *
-	 * @param WC_Order|WC_Order_Refund $order
-	 *
-	 * @return array|stdClass
-	 * @throws WC_YouCanPay_Exception
-	 */
-	public function create_source( $order ) {
-		$currency              = $order->get_currency();
-		$return_url            = $this->get_youcanpay_return_url( $order, self::ID );
-		$post_data             = [];
-		$post_data['amount']   = WC_YouCanPay_Helper::get_youcanpay_amount( $order->get_total(), $currency );
-		$post_data['currency'] = strtoupper( $currency );
-		$post_data['type']     = 'standalone';
-		$post_data['owner']    = $this->get_owner_details( $order );
-		$post_data['redirect'] = [ 'return_url' => $return_url ];
+    /**
+     * Creates the source for charge.
+     *
+     * @param WC_Order|WC_Order_Refund $order
+     *
+     * @return array|stdClass
+     * @throws WC_YouCanPay_Exception
+     */
+    public function create_source($order)
+    {
+        $currency = $order->get_currency();
+        $return_url = $this->get_youcanpay_return_url($order, self::ID);
+        $post_data = [
+            'amount'   => WC_YouCanPay_Helper::get_youcanpay_amount($order->get_total(), $currency),
+            'locale'   => WC_YouCanPay_Helper::get_supported_local(get_locale()),
+            'currency' => strtoupper($currency),
+            'type'     => 'standalone',
+            'owner'    => $this->get_owner_details($order),
+            'redirect' => ['return_url' => $return_url],
+        ];
 
-		WC_YouCanPay_Logger::info( 'Info: Begin creating YouCan Pay Standalone source' );
+        return WC_YouCanPay_API::request($order, $post_data);
+    }
 
-		return WC_YouCanPay_API::request( $order, $post_data );
-	}
+    /**
+     * Process the payment
+     *
+     * @param int $order_id Reference.
+     *
+     * @return array
+     */
+    public function process_payment($order_id)
+    {
+        try {
+            $order = wc_get_order($order_id);
 
-	/**
-	 * Process the payment
-	 *
-	 * @param int $order_id Reference.
-	 *
-	 * @return array
-	 */
-	public function process_payment( $order_id ) {
-		try {
-			$order = wc_get_order( $order_id );
+            // This will throw exception if not valid.
+            $this->validate_minimum_order_amount($order);
 
-			// This will throw exception if not valid.
-			$this->validate_minimum_order_amount( $order );
+            $response = $this->create_source($order);
 
-			$response = $this->create_source( $order );
+            $order->update_meta_data('_youcanpay_source_id', $response->id);
+            $order->save();
 
-			if ( ! empty( $response->error ) ) {
-				$order->add_order_note( $response->error->message );
+            WC_YouCanPay_Logger::info('Info: Redirecting to YouCan Pay Standalone...');
 
-				throw new WC_YouCanPay_Exception( print_r( $response, true ), $response->error->message );
-			}
+            return [
+                'result'   => 'success',
+                'redirect' => esc_url_raw($response->redirect->url),
+            ];
+        } catch (WC_YouCanPay_Exception $e) {
+            wc_add_notice($e->getLocalizedMessage(), 'error');
+        } catch (Throwable $e) {
+            wc_add_notice(__('Fatal error, please try again.', 'youcan-pay'), 'error');
+        }
 
-			$order->update_meta_data( '_youcanpay_source_id', $response->id );
-			$order->save();
+        if (isset($order)) {
+            $order->update_status('failed');
+        }
 
-			WC_YouCanPay_Logger::info( 'Info: Redirecting to YouCan Pay Standalone...' );
-
-			return [
-				'result'   => 'success',
-				'redirect' => esc_url_raw( $response->redirect->url ),
-			];
-		} catch ( WC_YouCanPay_Exception $e ) {
-			wc_add_notice( $e->getLocalizedMessage(), 'error' );
-			WC_YouCanPay_Logger::alert( 'wc youcan pay exception', array(
-				'exception.message' => $e->getMessage()
-			) );
-
-			$statuses = apply_filters(
-				'wc_youcanpay_allowed_payment_processing_statuses',
-				[ 'pending', 'failed' ]
-			);
-
-			if ( isset( $order ) ) {
-				$order->update_status( 'failed' );
-				if ( $order->has_status( $statuses ) ) {
-					$this->send_failed_order_email( $order_id );
-				}
-			}
-
-			return [
-				'result'   => 'fail',
-				'redirect' => '',
-			];
-		}
-	}
+        return [
+            'result'   => 'fail',
+            'redirect' => '',
+        ];
+    }
 
     /**
      * Returns all supported currencies for this payment method.
      *
      * @return array
      */
-    public function get_supported_currency() {
+    public function get_supported_currency()
+    {
         return WC_YouCanPay_Currencies::get_all_index();
     }
 }
